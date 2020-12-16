3ec1186483f411c11fce37f98e7d9f5afd39e601
zm <1875986169@qq.com>
2020-12-16 17:53:51 +0800

    初始导入,Person只有一个属性

---
+++ src/com/bjpowernode/domian/Person.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.domian;
+
+/**
+ * ClassName:Person
+ * Package:com.bjpowernode.domian
+ * Description:TODO
+ *
+ * @date:2020/12/16 17:00
+ * @author:zm
+ */
+public class Person {
+    private String id;
+}
